782fd06b398050f9ec28aa1822af87cc4d6efc10
Dust <dearestdust071@gmail.com>
2025-11-24 16:57:28 -0600

    Initial Commit

---
+++ SimuladorFisicas/SimuladorFisicasApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimuladorFisicasApp.swift
+//  SimuladorFisicas
+//
+//  Created by Jorge on 24/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SimuladorFisicasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimuladorFisicasTests/SimuladorFisicasTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimuladorFisicasTests.swift
+//  SimuladorFisicasTests
+//
+//  Created by Jorge on 24/11/25.
+//
+
+import Testing
+@testable import SimuladorFisicas
+
+struct SimuladorFisicasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
